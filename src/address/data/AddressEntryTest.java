feea0badf4bad3b2a6c850b263bb36f029b1e90b
Vane <vrodriguez89@horizon.csueastbay.edu>
2024-02-24 07:29:37 -0800

    Implemented testParameterizedConstructor().

--- src/address/data/AddressEntryTest.java
+++ src/address/data/AddressEntryTest.java
@@ -28,4 +28,24 @@ class AddressEntryTest {
         assertEquals("", addressEntry.getEmail());
     }
 
+    /**
+     * Tests parameterized constructor.
+     */
+    @org.junit.jupiter.api.Test
+    public void testParameterizedContructor()
+    {
+        AddressEntry addressEntry = new AddressEntry("John", "Doe",
+                "123 Main St", "Alameda", "California", 12345,
+                "111-1111", "john@example.com");
+
+        assertEquals("John", addressEntry.getFirstName());
+        assertEquals("Doe", addressEntry.getLastName());
+        assertEquals("123 Main St", addressEntry.getStreet());
+        assertEquals("Alameda", addressEntry.getCity());
+        assertEquals("California", addressEntry.getState());
+        assertEquals(12345, addressEntry.getZip());
+        assertEquals("111-1111", addressEntry.getPhone());
+        assertEquals("john@example.com", addressEntry.getEmail());
+    }
+
 }
