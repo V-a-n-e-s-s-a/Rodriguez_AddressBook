a0866623a8736f7a85b68adaf94cfabf0beda02f
Vane <vrodriguez89@horizon.csueastbay.edu>
2024-02-27 07:11:50 -0800

    Implemented AddressBookApplication.

--- src/address/AddressBookApplication.java
+++ src/address/AddressBookApplication.java
@@ -1,4 +1,33 @@
 package address;
 
-public class AddressBookApplication {
+import address.data.AddressBook;
+
+import java.io.FileNotFoundException;
+
+/**
+ * @author Vanessa Rodriguez
+ *
+ * AddressBookApplication is a class that uses Menu classes and AddressBook.
+ */
+
+public class AddressBookApplication
+{
+    /**
+     * The main method of the application.
+     *
+     * @param args is the command line arguments.
+     * @throws FileNotFoundException if a file is not found during file operations
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+
+        /**
+         * Creates an instance of AddressBook called addressBook.
+         */
+        AddressBook addressBook = new AddressBook();
+
+        /**
+         * Displays the menu using Menu class.
+         */
+        Menu.displayMenu(addressBook, "");
+    }
 }
