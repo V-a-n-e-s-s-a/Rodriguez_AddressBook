4a410e0389abb2a129f588fc3c0b10548a1669c8
Vane <vrodriguez89@horizon.csueastbay.edu>
2024-02-27 01:02:51 -0800

    Updated Menu.

--- src/address/Menu.java
+++ src/address/Menu.java
@@ -1,29 +1,157 @@
 package address;
 
+import address.data.AddressBook;
+import address.data.AddressEntry;
+
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * @author Vanessa Rodriguez
+ *
+ * Menu is a class used to display menu options to the user and various
+ * prompt methods.
+ */
 
 public class Menu {
 
     /**
      * Displays the menu to the screen.
+     * Handles all menu options.
      *
-     * @return the user's menu selection
+     * @param addressBook is the instance of AddressBook
+     * @param choice is the choice the user selects from menu
+     * @throws FileNotFoundException is the error thrown if the file is not found
      */
-    public static String displayMenu()
+    public static void displayMenu(AddressBook addressBook, String choice) throws FileNotFoundException
     {
+        boolean quit = false;
         Scanner scanChoice = new Scanner(System.in);
 
-        System.out.println("*************************\n" +
-                "Please enter your menu selection\n" +
-                "a) Loading From File\n" +
-                "b) Addition\n" +
-                "c) Removal\n" +
-                "d) Find\n" +
-                "e) Listing\n" +
-                "f) Quit\n" +
-                "*************************");
+        /**
+         * Quits when the user selects 'n'.
+         */
+        while (!quit)
+        {
+            /**
+             * Displays menu so that user can make their choice.
+             */
+            System.out.println("*************************\n" +
+                    "Please enter your menu selection\n" +
+                    "a) Loading From File\n" +
+                    "b) Addition\n" +
+                    "c) Removal\n" +
+                    "d) Find\n" +
+                    "e) Listing\n" +
+                    "f) Quit\n" +
+                    "*************************");
 
-        return scanChoice.nextLine();
+            choice = scanChoice.nextLine();
+
+            /**
+             * The user selected their choice.
+             */
+            switch (choice)
+            {
+                /**
+                 * User must enter file name(entries.txt) to load the file.
+                 */
+                case "a":
+                    Scanner scanFileName = new Scanner(System.in);
+                    System.out.println("Enter in FileName:\n");
+                    addressBook.readFromFile(scanFileName.nextLine());
+                    break;
+
+                /**
+                 * User has selected to add new entry to the AddressBook.
+                 */
+                case "b":
+                    String firstName = prompt_FirstName();
+                    String lastName = prompt_LastName();
+                    String street = prompt_Street();
+                    String city = prompt_City();
+                    String state = prompt_State();
+                    int zip = prompt_Zip();
+                    String phone = prompt_Phone();
+                    String email = prompt_Email();
+
+                    AddressEntry entry = new AddressEntry(firstName, lastName,
+                            street, city, state, zip, phone, email);
+
+                    addressBook.add(entry);
+                    break;
+
+                /**
+                 * User has selected to remove entry from the AddressBook
+                 * based on last name.
+                 */
+                case "c":
+                    /**
+                     * Finds the entries with same last name and displays them to the screen.
+                     */
+                    Scanner scanLastName = new Scanner(System.in);
+                    System.out.println("Enter in Last Name of contact to remove:\n");
+                    Set<AddressEntry> matchingEntries = addressBook.find(scanLastName.nextLine());
+
+                    System.out.println("The following entry was found in the address book.\n");
+                    for (AddressEntry foundEntry : matchingEntries)
+                    {
+                        System.out.println(foundEntry);
+                    }
+
+                    /**
+                     * Removes the entries if user decides to.
+                     */
+                    Scanner scanYesOrNo = new Scanner(System.in);
+                    System.out.println("Hit 'y' to remove the entry or 'n' to return to main menu\n");
+
+                    if (scanYesOrNo.nextLine() == "y")
+                    {
+                        for (AddressEntry foundEntry : matchingEntries)
+                        {
+                            addressBook.remove(scanLastName.nextLine());
+                        }
+                    }
+                    break;
+
+                /**
+                 * Finds an entry from the AddressBook based on last name.
+                 */
+                case "d":
+                    /**
+                     * Finds the entries with same last name and displays them to the screen.
+                     */
+                    Scanner scanFindLastName = new Scanner(System.in);
+                    System.out.println("Enter in all or beginning of last name you wish to find:\n");
+                    Set<AddressEntry> entries = addressBook.find(scanFindLastName.nextLine());
+
+                    System.out.println("The following entries were found in the address" +
+                            " book for a last name starting with" + scanFindLastName.nextLine() + "\n");
+
+                    for (AddressEntry foundEntry : entries)
+                    {
+                        System.out.println(foundEntry);
+                    }
+
+                    break;
+
+                /**
+                 * Lists the contents of AddressBook.
+                 */
+                case "e":
+                    addressBook.list();
+                    break;
+
+                /**
+                 * User decides to quit.
+                 */
+                case "f":
+                    System.out.println("Quitting.");
+                    quit = true;
+                    break;
+            }
+        }
     }
 
     /**
